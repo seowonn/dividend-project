06d59a38ef6ec588bf2300ad3eed6d30ade562f0
seowonn <sonseowon5@gmail.com>
2024-02-06 08:27:22 +0900

    feat : implement auth controller

---
+++ src/main/java/com/example/dividend/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.example.dividend.controller;
+
+import com.example.dividend.entity.MemberEntity;
+import com.example.dividend.model.Auth;
+import com.example.dividend.security.TokenProvider;
+import com.example.dividend.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final MemberService memberService;
+
+    private final TokenProvider tokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody Auth.SignUp request) {
+        MemberEntity memberEntity = this.memberService.register(request);
+        return ResponseEntity.ok(memberEntity);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> signin(@RequestBody Auth.SignIn request) {
+        MemberEntity member = this.memberService.authenticate(request);
+        String token = this.tokenProvider
+                        .generateToken(member.getUsername(), member.getRoles());
+        return ResponseEntity.ok(token);
+    }
+}
--- src/main/java/com/example/dividend/controller/AutoController.java
+++
@@ -1,7 +0,0 @@
-package com.example.dividend.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AutoController {
-}
